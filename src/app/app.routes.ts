78b3b3a6f4b7b4e9d0b0ba3208d8c4e5ec5d2236
Franco <franco.antuna.nob@gmail.com>
2023-11-29 20:37:27 -0300

    integracion del header, router y home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DetallesPokemonComponent } from './components/detalles-pokemon/detalles-pokemon.component';
+import { TablaPokemonComponent } from './components/tabla-pokemon/tabla-pokemon.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: TablaPokemonComponent},
+    {path: 'DetallesPokemon/id', component: DetallesPokemonComponent},
+    {path:'', pathMatch: 'full', redirectTo: 'home'},
+    {path:'**', pathMatch: 'full', redirectTo: 'home'},
+];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MaterialModule } from '../../shared/material.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+
+  }
+
+  home(){
+    this.router.navigateByUrl('/home')
+  }
 }
