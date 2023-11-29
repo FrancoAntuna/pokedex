c49fe906902afa06e5c0562fdace9ee280887576
Franco <franco.antuna.nob@gmail.com>
2023-11-29 19:51:11 -0300

    creacion de componentes y servicios

---
+++ src/app/components/detalles-pokemon/detalles-pokemon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles-pokemon',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalles-pokemon.component.html',
+  styleUrl: './detalles-pokemon.component.scss'
+})
+export class DetallesPokemonComponent {
+
+}
---
+++ src/app/components/tabla-pokemon/tabla-pokemon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-pokemon',
+  standalone: true,
+  imports: [],
+  templateUrl: './tabla-pokemon.component.html',
+  styleUrl: './tabla-pokemon.component.scss'
+})
+export class TablaPokemonComponent {
+
+}
